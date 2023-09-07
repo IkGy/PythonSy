a6bff7d3b9b146100d5eb2127105f66d988c5702
GYEONGIK <gmldi10009@gmail.com>
2023-09-07 13:38:22 +0900

    0907_1109_02

---
+++ list.py
@@ -0,0 +1,8 @@
+# test = list(range(0,30,1))
+# alist = [i for i in test if i % 2 == 0];
+
+# print(alist)
+            
+array = ['사과','자두','초콜릿','바나나']
+output = [fruit for fruit in array if fruit != '초콜릿']
+print(output)
